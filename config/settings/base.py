7caba3ec1277721ae85efe2c80f5bfee0439f3a9
Sairaj Jadhav <jadhavsairaj25@gmail.com>
2026-02-20 22:22:42 +0530

    modified:   config/settings/base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -105,15 +105,14 @@ DATABASES = {
 """
 
 
-
 DATABASES = {
-    'default': {
-        'ENGINE':'django.db.backends.mysql',
-        'NAME': 'Shopnow',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': '',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "3306"),
     }
 }
 
